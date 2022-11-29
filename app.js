af405aca041c482bc0ca0b326a851b64df966011
jamextech1 <okorojameschizaram@gmail.com>
2022-11-29 15:00:39 +0100

    ci/cd

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".hamburger");
+const closeNav = document.querySelector(".hamburger-x");
+const links = document.querySelector(".nav-links");
+const navLinks = document.querySelector(".nav-links a");
+btn.addEventListener("click", function (e) {
+  links.style.right = "0";
+});
+closeNav.addEventListener("click", function (e) {
+  links.style.right = "-1000px";
+});
